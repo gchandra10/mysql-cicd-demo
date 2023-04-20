4de8ef5f996ae5f8987d60fcb0f81f4ef4d3c7a8
Ganesh Chandrasekaran <gchandra@gmail.com>
2023-04-20 08:25:39 -0400

    MySQL Test

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Run the MySQL query and store the count in a variable
-count=$(mysql -u root  -h127.0.0.1 mysql -e "SELECT COUNT(*) FROM users WHERE name = 'John Doe';")
+count=$(mysql -u root  -h127.0.0.1 mysql -e "SELECT count(*) FROM users WHERE name = 'John Doe'")
 
 # Check if the count is 1 or 0 and output a message accordingly
 if [ $count -eq 1 ]; then
