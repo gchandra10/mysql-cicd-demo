ef4764c042e6ea869c85bdceea1a72745d4a39d0
Ganesh Chandrasekaran <gchandra@gmail.com>
2023-04-20 09:24:01 -0400

    MySQL Test

--- sp.sql
+++ sp.sql
@@ -1,4 +1,4 @@
 create procedure sp_insert(IN ip_name VARCHAR(50), IN ip_email VARCHAR(150))
 BEGIN
-    insert into users values (ip_name, ip_email, now());
+    insert into users values (ip_name, ip_email, CURRENT_TIMESTAMP());
 END;
