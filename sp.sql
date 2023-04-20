84a2b5a23ee844e72b9780b0253f775625003db8
Ganesh Chandrasekaran <gchandra@gmail.com>
2023-04-20 09:28:31 -0400

    MySQL Test

--- sp.sql
+++ sp.sql
@@ -1,6 +1,4 @@
-DELIMITER //
 create procedure sp_insert(IN ip_name VARCHAR(50), IN ip_email VARCHAR(150))
 BEGIN
-    INSERT INTO users VALUES (ip_name, ip_email, CURRENT_TIMESTAMP());
-END; //
-DELIMITER ;
+    INSERT INTO `users` (`name`, `email`, `created_at`) VALUES (ip_name, ip_email, CURRENT_TIMESTAMP());
+END; 
